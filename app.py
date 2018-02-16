fcf8e900afde8a541d17f8cb8ad4092670ea9b83
mwanjajoel <joelsilverworks@gmail.com>
2018-02-16 13:02:22 +0300

    changed the import statement

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, jsonify
-import flask_excel as excel
+from flask.ext import excel
 
 
 app = Flask(__name__)
@@ -15,7 +14,7 @@ def upload_file():
     return '''
     <!doctype html>
     <title>Upload an excel file</title>
-    <h1>Excel file upload (csv, tsv, csvz, xlsx, tsvz only)</h1>
+    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
     <form action="" method=post enctype=multipart/form-data><p>
     <input type=file name=file><input type=submit value=Upload>
     </form>
