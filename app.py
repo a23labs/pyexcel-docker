b6e66d066909a1e5f0dfb2e905fd5df3fe9f4ce0
Codebender <codebender@Codebenders-MacBook-Air.local>
2018-04-26 09:57:28 +0300

    Fixed missing packages in flask app and updated docker files

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 from flask import Flask, request, jsonify
 import flask_excel as excel
-from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app)
-
-
-@app.route('/')
-def hello_world():
-    return 'Know Your Rights!'
 
 
 @app.route("/upload", methods=['GET', 'POST'])
-@cross_origin()
 def upload_file():
     if request.method == 'POST':
-        return jsonify({"result": request.get_records(field_name='file')})
-        #return jsonify(excel.ExcelRequest.get_array())
+        return jsonify({"result": request.get_dict(field_name='file')})
     return '''
     <!doctype html>
-    <title>Upload an excel file</title>
-    <h1>Excel file upload (xls, xlsx, csv only)</h1>
+    <title>Upload an excel file here</title>
+    <h1>Excel file upload (csv, tsv, csvz, tsvz only)</h1>
     <form action="" method=post enctype=multipart/form-data><p>
     <input type=file name=file><input type=submit value=Upload>
     </form>
@@ -34,10 +25,17 @@ def download_file():
 
 @app.route("/export", methods=['GET'])
 def export_records():
-    return excel.make_response_from_array([[1, 2], [3, 4]], "csv", file_name="export_data")
+    return excel.make_response_from_array([[1, 2], [3, 4]], "csv",
+                                          file_name="export_data")
 
 
-# insert database related code h
+@app.route("/download_file_named_in_unicode", methods=['GET'])
+def download_file_named_in_unicode():
+    return excel.make_response_from_array([[1, 2], [3, 4]], "csv",
+                                          file_name=u"中文文件名")
 
-if __name__ == '__main__':
-    app.run()
+
+# insert database related code here
+if __name__ == "__main__":
+    excel.init_excel(app)
+    app.run(host='0.0.0.0')
